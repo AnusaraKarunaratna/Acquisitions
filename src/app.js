9f3639bee2da61e8664f38dead596948ca19c3a1
AnusaraKarunaratna <ansuarakarunaratna@gmail.com>
2026-05-21 18:19:28 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res)=> {
+    res.status(200).send('Hello from Acquisition API!');
+});
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,3 @@
+// To read environment variables from .env file
+import 'dotenv/config';
+import './server.js';
