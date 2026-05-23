cf15b30ca17c6227b049f48d1019b051e13b0437
AnusaraKarunaratna <ansuarakarunaratna@gmail.com>
2026-05-23 20:54:29 +0530

    Implement CI/CD pipelines and Github actions

---
+++ test/app.test.js
@@ -0,0 +1,27 @@
+import app from '#src/app.js';
+import request from 'supertest';
+
+describe('API Endpoints',() => {
+    describe('GET /health', () => {
+        it('should return health status', async () => {
+            const response = await request(app).get('/health').expect(200);
+            expect(response.body).toHaveProperty('status', 'OK');
+            expect(response.body).toHaveProperty('timestamp');
+            expect(response.body).toHaveProperty('uptime');
+        });
+    })
+
+    describe('GET /api', () => {
+        it('should return API messsage', async () => {
+            const response = await request(app).get('/api').expect(200);
+            expect(response.body).toHaveProperty('message', 'Acquisition API is running');
+        });
+    })
+
+    describe('GET /nonexistent', () => {
+        it('should return 404 error', async () => {
+            const response = await request(app).get('/nonexistent').expect(404);
+            expect(response.body).toHaveProperty('error', 'Route not found');
+        });
+    })
+})
